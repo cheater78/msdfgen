df6c4a91b1c08c812ebed392fdf83724bf9bd454
cheater78 <75250245+cheater78@users.noreply.github.com>
2024-12-01 21:29:39 +0100

    fixed non existing/nessecary msdfgen-config in core/base.h

--- core/base.h
+++ core/base.h
@@ -2,7 +2,7 @@
 #pragma once
 
 // This file needs to be included first for all MSDFgen sources
-
+#define MSDFGEN_PUBLIC /* just breaks everthing ifndef */
 #ifndef MSDFGEN_PUBLIC
 #include <msdfgen/msdfgen-config.h>
 #endif
